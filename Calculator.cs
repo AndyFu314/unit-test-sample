026f852b5cdecc4513fcb0d446d58a2696ea6ab8
Andy@UDN <andy.fu@udngroup.com.tw>
2016-11-01 17:34:25 +0800

    Modified parameter's name

--- Calculator.cs
+++ Calculator.cs
@@ -8,9 +8,9 @@ namespace UnitTestSample
 {
     public class Calculator:ICalculator
     {
-        public int Add(int firstNum, int secondNumber)
+        public int Add(int firstNum, int secondNum)
         {
-            return firstNum + secondNumber;
+            return firstNum + secondNum;
         }
 
         public int Subtract(int firstNum, int secondNum)
